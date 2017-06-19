b334cd10ea2ee27ee7c49f723994d3eab1861bae
z3t4 <jerome.sernin@gmail.com>
2017-06-19 20:59:25 +0200

    Basic IU
    Up Down event system with button.
    Map is not connected yet.

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    
+
+    public Renderer rend;
+    public Material standardMaterial;
+    public Material attackMaterial;
+
+    private Dictionary<string, float> cooldowns = new Dictionary<string, float>();
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
     public void onAttackEvent()
     {
+        this.rend.material = attackMaterial;
+        Debug.Log("Click");
+    }
 
+    public void onAttackEndEvent()
+    {
+        this.rend.material = standardMaterial;
+        Debug.Log("End Click");
     }
 
     public void onMagicEvent()
@@ -24,7 +38,17 @@ public class Player : MonoBehaviour {
 
     }
 
+    public void onMagicEndEvent()
+    {
+
+    }
+
     public void onHealEvent()
+    {
+
+    }
+
+    public void onHealEndEvent()
     {
 
     }
